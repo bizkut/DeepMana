82b6562587e7c06e5729d50d099bb206eb1ae9b8
Administrator <Administrator@WIN-01RDSH398FL>
2026-01-03 12:00:06 +0100

    Fix card effect triggers and imports for Cargo Guard and Wailing Vapor

--- card_effects/core/effect_CORE_WC_042.py
+++ card_effects/core/effect_CORE_WC_042.py
@@ -1,7 +1,9 @@
 """Effect for CORE_WC_042 in CORE"""
+from simulator.enums import CardType, Race
 
 def setup(game, source):
     def on_play_c(game, trig_src, card, target):
-        if card.controller == trig_src.controller and card.card_type == CardType.MINION and card.data.race == Race.ELEMENTAL:
+        if card.controller == trig_src.controller and card.card_type == CardType.MINION and hasattr(card.data, 'race') and card.data.race == Race.ELEMENTAL:
             trig_src.attack += 1
+            trig_src.health += 1 # Wailing Vapor is +1/+1
     game.register_trigger('on_card_played', source, on_play_c)
